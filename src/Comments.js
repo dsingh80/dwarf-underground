599b3a3ac93da57e55aa898333b97d23c8169efb
Seth Baughman <sbaughman1@me.com>
2017-05-23 14:58:11 -0400

    Add Comments Component

--- src/Comments.js
+++ src/Comments.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import './Comments.css'
 
 class Comments extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      comment: ''
+      comment: '',
+      comments: [],
     }
 
     this.updateComment = this.updateComment.bind(this)
+    this.addComment = this.addComment.bind(this)
   }
 
   updateComment(ev) {
@@ -16,14 +19,34 @@ class Comments extends Component {
     })
   }
 
+  addComment(ev) {
+    const state = {...this.state}
+    const comment = {
+      text: this.state.comment,
+      time: new Date()
+    }
+    state.comments.push(comment)
+    state.comment = ''
+    this.setState(state)
+  }
+
   render() {
     return (
       <div className="comments">
         <textarea value={this.state.comment} onChange={this.updateComment} placeholder="Enter comment here"></textarea>
-        <button className="button">Submit comment</button>
+        <button className="button" onClick={this.addComment}>Submit comment</button>
+        {this.state.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
       </div>
     )
   }
+}
+
+function Comment (props) {
+  return (
+    <div className="comment">
+      <div>{props.comment.text}</div>
+    </div>
+  )
 }
 
 export default Comments
