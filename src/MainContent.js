c9ec5223319401c1d540e710a500ba386ab54c01
Seth Baughman <sbaughman1@me.com>
2017-05-23 14:14:38 -0400

    Make OtherArticles and ArticleLink components

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react'
 import Article from './Article'
 import Ad from './Ad'
+import OtherArticles from './OtherArticles'
 import './MainContent.css'
+
+const otherArticles = [
+  {
+    imageUrl: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
+    caption: "Single Orcs in Indianapolis",
+    alt: "orcs"
+  },
+  {
+    imageUrl: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg",
+    caption: "You won't believe what's under this mountain",
+    alt: "mountain"
+  },
+  {
+    imageUrl: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
+    caption: "Mine 20% more gold with One Weird Trick",
+    alt: "gold"
+  },
+  {
+    imageUrl: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
+    caption: "Surprise for Indiana Hobbits born before 1999",
+    alt: "hobbit"
+  },
+]
 
 class MainContent extends Component {
   render() {
@@ -11,33 +35,7 @@ class MainContent extends Component {
         
         <Ad />
 
-        <div className="small-12 columns other-articles">
-          <h2>From around the Realm</h2>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-              <p>Single Orcs in Indianapolis</p>
-            </a>  
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-              <p>You won't believe what's under this mountain</p>
-            </a>  
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-              <p>Mine 20% more gold with One Weird Trick</p>
-            </a> 
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-              <p>Surprise for Indiana Hobbits born before 1999</p>
-            </a> 
-          </div>
-        </div>
+        <OtherArticles links={otherArticles} />
       </main>
     )
   }
