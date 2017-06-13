2ce89aab3d6a38b26c82a22f1b6688c156a4920d
="Damanveer Singh <=>
2017-06-13 14:13:05 -0400

    Added Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
-
+import './Navbar.css';
 class Navbar extends Component{
     render(){
         return (
             <div className="clearfix">
                 <div className="float-left logo">
-                <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
+                    <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
                 </div>
                 <div className="float-right nav-links">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Log in</a>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Log in</a>
                 </div>
             </div>
         )
